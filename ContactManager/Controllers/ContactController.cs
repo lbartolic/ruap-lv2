f75a61877795f8c31c7ef6cc5daddbec20bdd35d
unknown <lbartolic93@gmail.com>
2016-04-22 08:38:56 +0200

    Added contact manager controller

--- ContactManager/Controllers/ContactController.cs
+++ ContactManager/Controllers/ContactController.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace ContactManager.Controllers
 {
-    public class ContactController : Controller
+    public class ContactController : ApiController
     {
-        //
-        // GET: /Contact/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public string[] Get()
         {
             return new string[] {
